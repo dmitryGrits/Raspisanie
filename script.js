eb4cccc19607e68b412e817a91b88c86110f4395
DGRITS <grizay@icloud.com>
2024-05-10 14:43:06 +0300

    3,4 task

--- script.js
+++ script.js
@@ -3,16 +3,16 @@
 // JSON данные
 const lessonsData = [
     {
-        name: "Название занятия",
-        time: "Время провидения",
-        maxMembers: "Максимальное количество участников",
-        currentMembers: "Текущее количество участников"
+        name: "Бокс",
+        time: "12.05.2024 14:00",
+        maxMembers: 10,
+        currentMembers: 8
     },
     {
-        name: "Название занятия",
-        time: "Время провидения",
-        maxMembers: "Максимальное количество участников",
-        currentMembers: "Текущее количество участников"
+        name: "Плавание",
+        time: "11.05.2024 13:00",
+        maxMembers: 10,
+        currentMembers: 9
     }
 ];
 
@@ -31,27 +31,60 @@ function createLesson (name, time, maxMembers, currentMembers) {
 
     const lessonTime = document.createElement('p');
     lessonTime.textContent = time;
+    lessonTime.classList.add('time');
 
     const lessonMaxMembers = document.createElement('p');
     lessonMaxMembers.textContent = maxMembers;
+    lessonMaxMembers.classList.add('max');
 
     const lessonCurrentMembers = document.createElement('p');
     lessonCurrentMembers.textContent = currentMembers;
+    lessonCurrentMembers.classList.add('current');
 
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Редактировать';
-    editButton.classList.add('btn', 'btn-warning');
+    const signUpButton = document.createElement('button');
+    signUpButton.textContent = 'Записаться';
+    signUpButton.classList.add('sign','btn', 'btn-warning');
 
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Удалить';
-    deleteButton.classList.add('btn', 'btn-danger');
+    const cancelButton = document.createElement('button');
+    cancelButton.textContent = 'Отменить запись';
+    cancelButton.classList.add('cancel','btn', 'btn-danger');
 
     lessonItem.append(lessonName);
     lessonItem.append(lessonTime);
     lessonItem.append(lessonMaxMembers);
     lessonItem.append(lessonCurrentMembers);
-    lessonItem.append(editButton);
-    lessonItem.append(deleteButton);
+    lessonItem.append(signUpButton);
+    lessonItem.append(cancelButton);
 
     return lessonItem;
 }
+
+lessonList.addEventListener('click', function (e) {
+    
+    if (e.target.textContent == 'Записаться') {
+        const lessonItem = e.target.closest('li');
+        const lessonMaxMembers = lessonItem.querySelector('p.max');
+        const lessonCurrentMembers = lessonItem.querySelector('p.current');
+        const signUpButton = lessonItem.querySelector('button.sign');
+        const cancelButton = lessonItem.querySelector('button.cancel');
+        let current = Number(lessonCurrentMembers.textContent);
+        let max = Number(lessonMaxMembers.textContent);
+
+        if (current < max) {
+            signUpButton.classList.remove('btn-secondary');
+            signUpButton.classList.add('btn-warning');
+            current += 1;
+            lessonCurrentMembers.textContent = current;
+            alert("Вы записались на занятие!");
+            
+        } else {
+            signUpButton.classList.remove('btn-warning');
+            signUpButton.classList.add('btn-secondary');
+            alert("Вы не можете записаться занятие!");
+        }
+    
+    }
+
+
+
+});
